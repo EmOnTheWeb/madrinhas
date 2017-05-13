8f1657405e3d305b89befd44c16da845f2f4ff97
EmOnTheWeb <emilie.dannenberg@gmail.com>
2017-05-13 19:02:18 +0100

    finished that page nearly just sort out js!!

--- wp-content/themes/jupiter/views/singular/wp-success.php
+++ wp-content/themes/jupiter/views/singular/wp-success.php
@@ -13,20 +13,20 @@ if (have_posts())
 
 ?>
 
-<div class = 'graduation-background'>
+<div class = 'custom-container'>	
 	<div class = 'graduation'>
-	<!-- 	<i class="fa fa-graduation-cap" aria-hidden="true"></i> -->
+		<!-- 	<i class="fa fa-graduation-cap" aria-hidden="true"></i> -->
 
-		<div class = 'graduation__text'>
-		<?php $uploads = wp_upload_dir(); ?>
-		
-			<span class = 'text__number' ><img src='<?php echo $uploads['baseurl']; ?>/2017/fancy/circles.png'><span class='count'>9</span> </span>of our mentees have graduated
+			<div class = 'graduation__text'>
+			<?php $uploads = wp_upload_dir(); ?>
+			
+				<span class = 'text__number' ><img src='<?php echo $uploads['baseurl']; ?>/2017/fancy/circles.png'><span class='count'>9</span> </span>of our mentees have graduated
+
+			</div>
 
 		</div>
-
 	</div>
-</div>
-<div class = 'custom-container'>
+	<div class = 'white-background'>
 	<div class = 'specialization'>
 		<div class = 'half-width'>
 			<i class="fa fa-graduation-cap specicalization__icon" aria-hidden="true"></i>
@@ -78,13 +78,8 @@ if (have_posts())
 			</div>
 		</div>
 	</div>
+	</div>
 </div>
-
-	    Where Do We Go From Here?
-
-Alumni
-
-This year we see 5 new graduates from our Madrinha sponsorship. Together with 4 previous graduates, we will kick start the Madrinha Alumni. They will be the future mentors and co-mentors , giving back to future mentees . Hopefully, this will be the beginning of the Madrinha legacy of giving forward, and giving back.
 	<?php 
 	    do_action('mk_page_after_content');
 		?>
