85abe9dbc19ed99e4e615bb1dfb96c97106cd609
EmOnTheWeb <emilie.dannenberg@gmail.com>
2017-05-20 12:22:31 +0100

    added mentor mentee grid bit

--- wp-content/themes/jupiter/views/singular/wp-home.php
+++ wp-content/themes/jupiter/views/singular/wp-home.php
@@ -17,6 +17,32 @@ if (have_posts())
 		do_action('mk_page_before_content');
 
 	    the_content();
+
+?>
+<div class="mentor-mentee-grid-section">
+	<h2>We know every student we sponsor</h2>
+
+	<p class="p1">Once selected, each mentee is matched with a mentor. A mentor provides support throughout the mentees academic career, monitors their progress, and helps them realize their full potential. </p>
+	    
+	<div class="half-width">
+		<div class = "potrait-mentee">
+
+		 	<div class = "caption-bar">A mentee!</div>
+		</div>
+	</div>
+	<i class="fa fa-handshake-o" aria-hidden="true"></i>
+
+	<div class="half-width">
+		<div class = "potrait-mentor">
+
+		<div class = "caption-bar">A mentor!</div>
+		</div>
+	</div>
+</div>
+
+
+
+<?php 
 	    do_action('mk_page_after_content');
 		?>
 		<div class="clearboth"></div>
