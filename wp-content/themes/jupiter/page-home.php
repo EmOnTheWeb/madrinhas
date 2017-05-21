5fdb94eed83958d87df8bc9f15c9f4f81f955352
EmOnTheWeb <emilie.dannenberg@gmail.com>
2017-05-21 11:44:58 +0100

    change some links and hover state

--- wp-content/themes/jupiter/page-home.php
+++ wp-content/themes/jupiter/page-home.php
@@ -13,9 +13,10 @@ mk_build_main_wrapper( mk_get_view('singular', 'wp-home', true) );
 	<quote>"if you think you’re too small to have an impact, try going to bed with a mosquito in the room”</quote><br>
 	<span class="attribution">&nbsp; - Anita Roddick </span>
 </div>
+<?php $uploads = wp_upload_dir(); ?>
 <section class="where-we-work">
-	<p class="where-we-work__text homepage__location-text">Though our focus is in West Africa, we sponsor students <a href="http://localhost:8888/who-do-we-support/">our mentees</a> from all over the world. We do this with the help of our <a href="http://localhost:8888/who-do-we-support/">partner charities</a></p>
-	<img src="http://localhost:8888/wp-content/uploads/2017/05/africa.png" alt="africa map" width="100%">
+	<p class="where-we-work__text homepage__location-text">Though our focus is in West Africa, we sponsor students <a href="/beneficiaries/mentees/">our mentees</a> from all over the world. We do this with the help of our <a href="/beneficiaries/our-partners">partner charities</a></p>
+	<img src="<?php echo $uploads['baseurl']; ?>/2017/05/africa.png" alt="africa map" width="100%">
 
 
 </section>
