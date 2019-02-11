2ce0dab8d29e8b5c7c7843ffcdff2e71fb2b182d
emilie.dannenberg@gmail.com <emilie.dannenberg@gmail.com>
2019-02-11 17:57:37 -0600

    add buttons with some suspect inline styles

--- wp-content/themes/jupiter/page-mentee-map.php
+++ wp-content/themes/jupiter/page-mentee-map.php
@@ -7,6 +7,15 @@ Mk_Static_Files::addAssets('mk_button');
 Mk_Static_Files::addAssets('mk_audio');
 Mk_Static_Files::addAssets('mk_swipe_slideshow');
 
+echo '<section style="padding: 60px 80px;font-size:22px;font-weight:500;line-height:1.2;">'; 
+echo 'We currently sponsor 77 mentees, including 37 new mentees in 2018/19'; 
+
+echo '</section>'; 
+
+echo '<section style="background-color:white; padding:40px 80px;"><a class="slick-style-btn">View mentees by subject</a><a class="slick-style-btn">View mentees by country</a></section>'; 
+
+mk_build_main_wrapper( mk_get_view('singular', 'wp-mentee-map', true) );
+
 mk_build_main_wrapper( mk_get_view('singular', 'wp-mentee-map', true) );
 
 get_footer();
