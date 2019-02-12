a176685310b4883f36bcee3f777e211d6a1222f8
emilie.dannenberg@gmail.com <emilie.dannenberg@gmail.com>
2019-02-11 20:26:25 -0600

    sort your lines and text out. now adding desktop version

--- wp-content/themes/jupiter/views/singular/wp-mentee-study-overview.php
+++ wp-content/themes/jupiter/views/singular/wp-mentee-study-overview.php
@@ -1,5 +1,121 @@
 <?php $uploads = wp_upload_dir(); ?>
-<section class='mentee-study'>
-	<img class='mentee-study__img' src='<?php echo $uploads['baseurl']; ?>/2017/fancy/circles.png'>
-	<hr class="one"><hr class="two"><hr class="three"><hr class="four"><hr class="five"><hr class="six"><hr class="seven"><hr class="eight"><hr class="nine"><hr class="ten"><hr class="eleven"><hr class="twelve"><hr class="thirteen">
+<section class="mentee-study">
+	<div class="desktop">
+		<img class="mentee-study__img" src='<?php echo $uploads['baseurl']; ?>/2017/fancy/circles.png'>
+		<hr class="one"><hr class="two"><hr class="three"><hr class="four"><hr class="five"><hr class="six"><hr class="seven"><hr class="eight"><hr class="nine"><hr class="ten"><hr class="eleven"><hr class="twelve"><hr class="thirteen">
+		<div class="one"><span class="num">3</span> Economics</div> 
+		<div class="two"><span class="num">8</span> Teaching</div> 
+		<div class="three"><span class="num">27</span>  Medicine/Medical Science</div> 
+		<div class="four"><span class="num">1</span> Tourism</div> 
+		<div class="five"><span class="num">5</span> IT</div> 
+		<div class="six"><span class="num">8</span> Business</div> 
+		<div class="seven"><span class="num">4</span> Accounting</div> 
+		<div class="eight"><span class="num">4</span> Social Work</div> 
+		<div class="nine"><span class="num">7</span> Electrical/Civil Engineering</div> 
+		<div class="ten"><span class="num">4</span> Environmental Science</div> 
+		<div class="eleven"><span class="num">6</span> Science and Math</div> 
+		<div class="twelve"><span class="num">1</span> International Studies</div> 
+		<div class="thirteen"><span class="num">1</span> Law</div> 
+	</div>
+
+	<div class="mobile">
+		<div class = 'specialization-stats-background'>
+			<div class = "custom-container specialization-stats">
+				
+					<div class = 'specialization'>
+						<div class = 'half-width'>
+							<i class="fa fa-graduation-cap specicalization__icon" aria-hidden="true"></i>
+
+							<div class = 'specialization__text'>
+								<span class = 'text__number'><span class='count'>4</span></span>  Graduated in <br><span class = 'text__specialization'>teaching</span>
+
+							</div>
+						</div>
+
+						<div class = 'half-width'>
+							<i class="fa fa-briefcase specicalization__icon" aria-hidden="true"></i>
+
+							<div class = 'specialization__text'>
+								<span class = 'text__number'><span class='count'>2</span></span>  Graduated in <br><span class = 'text__specialization'>business studies</span>
+
+							</div>
+						</div>
+					</div>
+
+					<div class = 'specialization'>
+						<div class = 'half-width'>
+							<i class="fa fa-stethoscope specicalization__icon" aria-hidden="true"></i>
+
+
+							<div class = 'specialization__text'>
+								<span class = 'text__number'><span class='count'>8</span></span>  Graduated in <br><span class = 'text__specialization'>medicine / medical science</span>
+
+							</div>
+						</div>
+
+						<div class = 'half-width'>
+							<i class="fa fa-users specicalization__icon" aria-hidden="true"></i>
+
+							<div class = 'specialization__text'>
+								<span class = 'text__number'><span class='count'>2</span></span>  Graduated in <br><span class = 'text__specialization'>performing arts</span>
+
+							</div>
+						</div>
+					</div>
+
+					<div class = 'specialization'>
+						<div class = 'half-width'>
+							<i class="fa fa-medkit specicalization__icon" aria-hidden="true"></i>
+
+							<div class = 'specialization__text'>
+								<span class = 'text__number'><span class='count'>1</span></span>  Graduated in <br><span class = 'text__specialization'>nursing</span>
+
+							</div>
+						</div>
+						<div class = 'half-width'>
+							<i class="fa fa-paw specicalization__icon" aria-hidden="true"></i>
+
+							<div class = 'specialization__text'>
+								<span class = 'text__number'><span class='count'>1</span></span>  Graduated in <br><span class = 'text__specialization'>veterinary science</span>
+
+							</div>
+						</div>
+					</div>
+
+					<div class = 'specialization'>
+						<div class = 'half-width'>
+							<i class="fa fa-wrench specicalization__icon" aria-hidden="true"></i>
+
+							<div class = 'specialization__text'>
+								<span class = 'text__number'><span class='count'>2</span></span>  Graduated in <br><span class = 'text__specialization'>Engineering</span>
+
+							</div>
+						</div>
+						<div class = 'half-width'>
+							<i class="fa fa-building specicalization__icon" aria-hidden="true"></i>
+
+							<div class = 'specialization__text'>
+								<span class = 'text__number'><span class='count'>1</span></span>  Graduated in <br><span class = 'text__specialization'>Interior Design</span>
+
+							</div>
+						</div>
+					</div>
+
+					<div class = 'specialization'>
+						<div class = 'half-width'>
+							<i class="fa fa-line-chart specicalization__icon" aria-hidden="true"></i>
+
+							<div class = 'specialization__text'>
+								<span class = 'text__number'><span class='count'>1</span></span>  Graduated in <br><span class = 'text__specialization'>Media Studies</span>
+
+							</div>
+						</div>
+						<div class = 'half-width'>
+							
+						</div>
+					</div>
+			
+			</div>
+		</div>
+	</div>
 </section>
