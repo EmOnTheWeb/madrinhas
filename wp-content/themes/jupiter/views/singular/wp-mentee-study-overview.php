a33732e0620fe999f9686e35b7541fb5bfc06cb8
emilie.dannenberg@gmail.com <emilie.dannenberg@gmail.com>
2019-02-11 19:19:52 -0600

    add lines

---
+++ wp-content/themes/jupiter/views/singular/wp-mentee-study-overview.php
@@ -0,0 +1,5 @@
+<?php $uploads = wp_upload_dir(); ?>
+<section class='mentee-study'>
+	<img class='mentee-study__img' src='<?php echo $uploads['baseurl']; ?>/2017/fancy/circles.png'>
+	<hr class="one"><hr class="two"><hr class="three"><hr class="four"><hr class="five"><hr class="six"><hr class="seven"><hr class="eight"><hr class="nine"><hr class="ten"><hr class="eleven"><hr class="twelve"><hr class="thirteen">
+</section>
