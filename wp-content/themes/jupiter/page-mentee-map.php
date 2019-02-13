92db662c27b2da7754288ba5cbbf77965661befd
emilie.dannenberg@gmail.com <emilie.dannenberg@gmail.com>
2019-02-12 19:19:58 -0600

    got to make colors in keeping...

--- wp-content/themes/jupiter/page-mentee-map.php
+++ wp-content/themes/jupiter/page-mentee-map.php
@@ -7,7 +7,7 @@ Mk_Static_Files::addAssets('mk_button');
 Mk_Static_Files::addAssets('mk_audio');
 Mk_Static_Files::addAssets('mk_swipe_slideshow');
 
-echo '<section style="padding: 60px 80px;font-size:22px;font-weight:500;line-height:1.2;">'; 
+echo '<section style="padding: 60px 80px;font-size:22px;font-weight:500;line-height:1.2;background-color:#3859cc;color:white;">'; 
 echo 'We currently sponsor 77 mentees, including 37 new mentees in 2018/19'; 
 
 echo '</section>'; 
@@ -19,7 +19,7 @@ echo '<section style="background-color:white; padding:40px 80px;">
 
 mk_build_main_wrapper( mk_get_view('singular', 'wp-mentee-study-overview', true) );
 
-echo '<section style="padding: 60px 80px;font-size:22px;font-weight:500;line-height:1.2;">'; 
+echo '<section style="padding: 60px 80px;font-size:22px;font-weight:500;line-height:1.2;background-color:#3859cc;color:white;">'; 
 echo '<a name="by-country"></a>'; 
 echo 'We operate with 13 partners out of 10 countries'; 
 
